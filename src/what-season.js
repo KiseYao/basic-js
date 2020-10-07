5dc12bffdb10a5445a97d56489da96d23d855e01
KiseYao <71172206+KiseYao@users.noreply.github.com>
2020-10-07 14:06:15 +0300

    feat: add solution

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,55 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function getSeason(date) {
+	
+	if (!date) return 'Unable to determine the time of year!';
+	 if (date.hasOwnProperty('getMonth')) throw Error;
+	
+    switch(date.getMonth()) {
+        case 'jan':
+        case 'january':
+        case 0:
+        case 'feb':
+        case 'february':
+        case 1:
+              return 'winter';
+        break;
+        case 'mar':
+        case 'march':
+        case 2:
+        case 'apr':
+        case 'april':
+        case 3:
+        case 'may':
+        case 4:
+           return 'spring';
+        break;
+        case 'jun':
+        case 'june':
+        case  5:
+        case 'jul':
+        case 'july':
+        case  6:
+        case 'aug':
+        case 'august':
+        case  7:
+          return 'summer';
+        break;
+        case 'sep':
+        case 'september':
+        case 8:
+        case 'oct':
+        case 'october':
+        case 9:
+        case 'nov':
+        case 'november':
+        case 10:
+            return'fall';
+        break;
+		 case 'dec':
+        case 'december':
+		case 11:
+		return 'winter'
+		break;
+    }
+}
